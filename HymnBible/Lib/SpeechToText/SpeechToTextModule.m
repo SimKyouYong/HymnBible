b5f829c35ac376f0871493a66b8ebce110e9d99b
JC1_joseph <josephkang12@gmail.com>
2017-04-27 22:12:28 +0900

    4/27 Commit 1.intro

--- HymnBible/Lib/SpeechToText/SpeechToTextModule.m
+++ HymnBible/Lib/SpeechToText/SpeechToTextModule.m
@@ -14,7 +14,8 @@
 #define	SAFE_RELEASE(ptr)	{if(ptr!=nil){if ([ptr respondsToSelector:@selector(setDelegate:)]) {[ptr performSelector:@selector(setDelegate:) withObject:nil];}[ptr release];ptr=nil;}}
 
 #define FRAME_SIZE 110
-#define GOOGLE_SPEECH_TO_TEXT_KEY @"AIzaSyDNsfgrFeAXOqqOJmbdNX3MWZ5t-xzjt3U"//@"AIzaSyDN-pxLQg_eeo0XJc9bnxGR_m1Gd21eZKU"
+#define GOOGLE_SPEECH_TO_TEXT_KEY @"AIzaSyDNsfgrFeAXOqqOJmbdNX3MWZ5t-xzjt3U"
+//#define GOOGLE_SPEECH_TO_TEXT_KEY @"AIzaSyC7oqpkvJlH8ltg3iydNve16yOS79SJcPg"
 #define GARBAGE_RESULT_VALUE @"{\"result\":[]}"
 
 @interface SpeechToTextModule ()
